34b079c2cb3172a3fa65c68f6484e0baa8c82e80
Cloud User <centos@ip-172-31-10-114.eu-west-2.compute.internal>
2020-06-16 11:56:59 +0000

    In progress. Working on initialising module's variables in testbench

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name: Emily Widjaja
+// Date: 16 June 2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -13,3 +13,68 @@
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module traffic(
+	input clk,
+	output reg red,
+	output reg amber,
+	output reg green
+  );
+
+  //add registers and wires
+//maybe something to combine the output into input? since old 
+	wire [2:0] current_signal;
+
+  //add user logic
+	assign current_signal[0] = red;
+	assign current_signal[1] = amber;
+	assign current_signal[2] = green; //current_signal might need to be reg idk.
+
+	always @ (posedge clk)
+	  begin
+
+	  //legal state 1: red -> red,amber
+	  if (current_signal==3'b001)
+//(current_signal[0]==1) && (current_signal[2:1]==0) ) 
+	  begin
+	    red<=1;	//technically this is unnecessary since already red
+	    amber<=1;
+	    green<=0;
+	  end
+
+	  //legal state 2: red,amber -> green
+	  if (current_signal==3'b011)
+//(current_signal[1:0]==1) && (current_signal[2]==0) ) 
+	  begin
+	    red<=0;
+	    amber<=0;
+	    green<=1;
+	  end
+
+	  //legal state 3: green -> amber
+	  if (current_signal==3'b100)
+//(current_signal[1:0]==0) && (current_signal[2]==1) )
+	  begin
+	    green<=0;
+	    amber<=1;	//could add in red<=0;
+	  end
+
+	  //legal state 4: amber -> red
+	  if (current_signal==3'b010)
+//(current_signal[1]==1) && (current_signal[2]==0) && (current_signal[0]==0) )
+	  begin
+	    amber<=0;
+	    red<=1; //could add in green<=0;
+	  end
+
+	  //illegal states. Assign default (red)
+	  else begin
+	    red<=1;
+	    amber<=0;
+	    green<=0;
+	  end
+
+	end //always
+
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,91 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name: Emily Widjaja
+// Date: 16 June 2020
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+//Parameters
+	parameter CLK_PERIOD = 10;
+
+//Registers and wires
+	reg clk;
+	wire red;
+	wire amber;
+	wire green;
+	reg [2:0] current_signal;
+	reg [2:0] prev_signal;
+	reg err;
+
+//Generate clock
+	initial
+	 begin
+	  clk = 1'b1;
+	  forever
+	   #(CLK_PERIOD/2) clk = ~clk;
+	 end
+
+//user logic
+	assign red = current_signal[0];
+	assign amber = current_signal[1];
+	assign green = current_signal[2];
+
+	initial begin
+	  err=0;
+	  //initialise current signal as red
+	  current_signal[0] = 1;	//red
+	  current_signal[1] = 0;  //amber
+	  current_signal[2] = 0;  //green
+	  prev_signal = current_signal;
+	  #1
+	  forever begin
+	    #CLK_PERIOD
+
+	  //check that is a legal state
+	    if (((current_signal==3'b001) || (current_signal==3'b011) || (current_signal==3'b100) || (current_signal==3'b010))==0) begin
+	      $display("***TEST FAILED! This is an illegal state! red=%d, amber=%d, green=%d, current signal=%b***",red,amber,green,current_signal);
+	      err=1;
+	    end
+
+	  //check that it has the correct sequence
+	    else begin
+	      if ( ((prev_signal==3'b001) && (current_signal!=3'b011)) || ((prev_signal==3'b011) && (current_signal!=3'b100)) || ((prev_signal==3'b100) && (current_signal!=3'b010)) || ((prev_signal==3'b010) &&(current_signal!=3'b001)) )
+	      begin
+		$display("*** Test FAILED! Incorrect sequence! current signal=%b, previous signal=%b***",current_signal,prev_signal);
+		err=1;
+	      end
+	    end //else
+
+	  prev_signal = current_signal;
+	  current_signal[0] = red;
+	  current_signal[1] = amber;
+	  current_signal[2] = green;
+	  end //forever
+
+	end //initial
+
+//Finish test, check for success
+    initial begin
+	#200
+	if (err==0)
+	 $display("***TEST PASSED!***");
+	$finish;
+    end
+
+//Instantiate counter module
+traffic top(
+  .clk (clk),
+  .red (red),
+  .amber (amber),
+  .green (green)
+  );
+endmodule
+	  
