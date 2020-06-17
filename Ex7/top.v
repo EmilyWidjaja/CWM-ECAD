31eef1773af1eee565c1c83502ffe4ad4d788967
Cloud User <centos@ip-172-31-10-114.eu-west-2.compute.internal>
2020-06-17 11:55:44 +0000

    In progress

--- Ex7/top.v
+++ Ex7/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #7
-// Student Name:
-// Date: 
+// Student Name: Emily Widjaja
+// Date: 17 June 2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory.
@@ -10,7 +10,38 @@
 //           clk, a[2:0], b[2:0], read
 //
 //  outputs:
-//           result[4:0]
+//           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
 
+module multiplication(
+	input clk,
+	input [2:0] a,
+	input [2:0] b,
+	input read,
+	output reg [5:0] result
+  );
+
+//add intermediate wires
+	wire wea
+	wire [5:0] dina
+
+//add user logic
+	assign dina = 6'b0;
+	assign wea = 0;
+
+	//always @ (posedge clk) begin
+	 //if (read);
+
+//instantiating memory block
+blk_mem_gen_0 mymem (
+  .clka(clk),    // input wire clka
+  .ena(read),      // input wire ena
+  .wea(wea),      // input wire [0 : 0] wea
+  .addra({a,b}),  // input wire [5 : 0] addra
+  .dina(dina),    // input wire [5 : 0] dina
+  .douta(result)  // output wire [5 : 0] douta
+);
+
+	
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -1,9 +1,91 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #7 - Times Table
-// Student Name:
-// Date: 
+// Student Name: Emily Widjaja
+// Date: 17 June 2020
 //
 // Description: A testbench module to test Ex7 - Times Table
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+//Parameters
+	parameter CLK_PERIOD=10;
+
+//Registers and wires
+	reg [2:0] a;
+	reg [2:0] b;
+	reg clk;
+	wire [2:0] result;
+	reg err;
+	reg read;
+
+//Generate clocks
+	//general clk & read clock
+	initial begin
+	  clk = 1'b1;
+	  read = 1'b1;
+	  forever begin
+	   #(CLK_PERIOD/2) clk = ~clk;
+	   #(CLK_PERIOD/2) read = ~read;
+	  end
+	end
+
+	//a input. For first test, will test for 3 times tables, so a will be constant and initialised in 'initialise'
+	 
+	  
+	//b input. Counts up then resets to 0.
+	initial begin
+	  b = 3'b000;
+	  forever begin
+	    if (b<=3'b111)
+	     b=b+3'b001;
+	    else
+	      b=3'b000;
+	  end
+	end
+	
+
+//user logic
+	//initialise
+	initial begin
+	  err=0;
+	  a = 3'b011; //tests 3 times tables
+	  #1
+
+	//forever, display a & b and result and check by eye if correct. If any undefined states, then failed. 
+	  forever begin
+	    #CLK_PERIOD
+	    $display("***a=%d,b=%d,result=%d***",a,b,result);
+	  
+	    if (result==1'bX) begin
+	      $display("***TEST FAILED! Result is undefined. a=%d,b=%d,result=%d***",a,b,result);
+	      err=1;
+	    end
+	  end //forever
+	end //initial
+
+//Finish test, check for success
+    initial begin
+	#200
+	if (err==0)
+	 $display("***TEST PASSED!***");
+	$finish;
+    end
+
+//instantiate module
+multiplication top (
+  .clk (clk),
+  .a (a),
+  .b (b),
+  .read (read),
+  .result (result)
+  );
+
+
+endmodule
+
 
