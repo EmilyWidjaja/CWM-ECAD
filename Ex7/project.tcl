cb55ae6a30fd8f23befa9c2d43d2d81e50d7d97c
Cloud User <centos@ip-172-31-10-114.eu-west-2.compute.internal>
2020-06-18 08:09:22 +0000

    In progress

--- Ex7/project.tcl
+++ Ex7/project.tcl
@@ -22,8 +22,7 @@ create_fileset -constrset -quiet constraints
 #Todo: Add your IP here
 create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name blk_mem_gen_0
 set_property -dict [list CONFIG.Write_Width_A {6} CONFIG.Write_Depth_A {64} CONFIG.Read_Width_A {6} CONFIG.Write_Width_B {6} CONFIG.Read_Width_B {6} CONFIG.Load_Init_File {true} CONFIG.Coe_File {/home/centos/CWM-ECAD/Ex7/mymem_init.coe}] [get_ips blk_mem_gen_0]
-reset_targetall [get_ips home/centos/CWM-ECAD/Ex7/Ex7.srcs/sources_1/ip/blk_mem_gen_0] #reset target command line taken from presentation
-
+reset_target all [get_ips home/centos/CWM-ECAD/Ex7/Ex7.srcs/sources_1/ip/blk_mem_gen_0]
 
 read_verilog "top.v"
 read_verilog "top_tb.v"
