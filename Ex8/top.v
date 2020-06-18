6cb91897f0053df4a95c7e66391759d42d7e7ee7
Cloud User <centos@ip-172-31-10-114.eu-west-2.compute.internal>
2020-06-18 10:53:39 +0000

    In progress

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
-// Date: 
+// Student Name: Emily Widjaja
+// Date: 18 June 2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory and AXI-4-lite interface.
@@ -12,3 +12,57 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module multiplication(
+	input clk,
+	input [2:0] a,
+	input [2:0] b,
+	input read,
+	input rst,
+	output [5:0] result
+  );
+
+//add intermediate wires
+	wire [31:0] address;
+	wire s_axi_aw_ready;
+	wire s_axi_wready;
+	wire s_axi_bvalid;
+	wire s_axi_bresp;
+	wire s_axi_arready;
+	wire rsta_busy;
+	wire rstb_busy;
+	wire [31:0] s_axi_ardata;
+
+//add user logic
+	//convert addresses
+	assign address = {24'b0,a,b,2'b00};
+	//assign result = s_axi_ardata[5:0];
+
+//instantiate memory block
+mymem mymemory (
+  .rsta_busy(rsta_busy),          // output wire rsta_busy
+  .rstb_busy(rstb_busy),          // output wire rstb_busy
+  .s_aclk(clk),                // input wire s_aclk
+  .s_aresetn(rst),          // input wire s_aresetn
+  .s_axi_awaddr(0),    // input wire [31 : 0] s_axi_awaddr	/not sure
+  .s_axi_awvalid(0),  // input wire s_axi_awvalid
+  .s_axi_awready(s_axi_awready),  // output wire s_axi_awready/
+  .s_axi_wdata(0),      // input wire [31 : 0] s_axi_wdata
+  .s_axi_wstrb(0),      // input wire [3 : 0] s_axi_wstrb
+  .s_axi_wvalid(0),    // input wire s_axi_wvalid	/cannot write?
+  .s_axi_wready(s_axi_wready),    // output wire s_axi_wready/
+  .s_axi_bresp(s_axi_bresp),      // output wire [1 : 0] s_axi_bresp/
+  .s_axi_bvalid(s_axi_bvalid),    // output wire s_axi_bvalid/
+  .s_axi_bready(0),    // input wire s_axi_bready
+  .s_axi_araddr(address),    // input wire [31 : 0] s_axi_araddr
+  .s_axi_arvalid(read),  // input wire s_axi_arvalid
+  .s_axi_arready(s_axi_arready),  // output wire s_axi_arready
+  .s_axi_rdata(s_axi_rdata),      // output wire [31 : 0] s_axi_rdata/
+  .s_axi_rresp(s_axi_rresp),      // output wire [1 : 0] s_axi_rresp
+  .s_axi_rvalid(s_axi_rvalid),    // output wire s_axi_rvalid
+  .s_axi_rready(read)    // input wire s_axi_rready
+);
+
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,103 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8 - Times Table using AXI interface
-// Student Name:
-// Date: 
+// Student Name: Emily Widjaja
+// Date: 18 June 2020
 //
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+//Parameters
+	parameter CLK_PERIOD=10;
+
+//Registers and wires
+	reg [2:0] a;
+	reg [2:0] b;
+	reg clk;
+	wire [5:0] result;
+	reg rst;
+	reg err;
+	reg read;
+
+//Generate clocks
+	//general clk & read clock
+	initial begin
+	  clk = 1'b1;
+	  #1
+	  forever begin
+	   #(CLK_PERIOD/2) clk = ~clk;
+	  end
+	end
+	
+	initial begin
+	  read = 1'b1;
+	  forever
+	  #(CLK_PERIOD/2) read = ~read;
+	end
+	//a input. For first test, will test for 7 times tables, so a will be constant and initialised in 'initialise'
+	 
+	  
+	//b input. Counts up then resets to 0.
+	initial begin
+	  b = 3'b000;
+	  #1
+	  forever begin
+	    #(CLK_PERIOD)
+	    if (b<=3'b111)
+	     b=b+3'b001;
+	    else
+	      b=3'b000;
+	  end
+	end
+	
+	//reset clock
+	initial begin
+	  rst = 1;
+	  #(CLK_PERIOD) rst = 0;
+	  #(10*CLK_PERIOD) rst = 1;
+	  #CLK_PERIOD rst = 0;
+	end
+
+//user logic
+	//initialise
+	initial begin
+	  err=0;
+	  a = 3'b111; //tests 7 times tables
+	  #1
+
+	//forever, display a & b and result and check by eye if correct. If any undefined states, then failed. 
+	  forever begin
+	    #CLK_PERIOD
+	    $display("***a=%d,b=%d,result=%d***",a,b,result);
+	  
+	    if (result==1'bX) begin
+	      $display("***TEST FAILED! Result is undefined. a=%d,b=%d,result=%d***",a,b,result);
+	      err=1;
+	    end
+	  end //forever
+	end //initial
+
+//Finish test, check for success
+    initial begin
+	#200
+	if (err==0)
+	 $display("***TEST PASSED!***");
+	$finish;
+    end
+
+//instantiate module
+multiplication top (
+  .clk (clk),
+  .a (a),
+  .b (b),
+  .read (read),
+  .result (result)
+  );
+
+
+endmodule
